c9772887af2451ef79ceb11edbcea9fb45b76419
dalysmedia <dalyjjjk@gmail.com>
2020-06-12 21:22:14 +0530

    Updates

---
+++ precache-manifest.df96d244a506e9804aee5ae3fa97919d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdbd50111a334f14c8a49cda5341855",
+    "url": "/dallysMedia/index.html"
+  },
+  {
+    "revision": "ea0a12a6016744bf99b4",
+    "url": "/dallysMedia/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/dallysMedia/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dallysMedia/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0a12a6016744bf99b4",
+    "url": "/dallysMedia/static/js/main.fd1998b8.chunk.js"
+  },
+  {
+    "revision": "4956dcf57dc1165def42",
+    "url": "/dallysMedia/static/js/runtime-main.61514994.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dallysMedia/static/media/logo.5d5d9eef.svg"
+  }
+]);
